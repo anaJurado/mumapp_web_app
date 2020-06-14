0021fc05847e3eaa544554d2acd6bbae71e48d8a
ana <am.juradoc@alumnos.urjc.es>
2020-06-14 20:46:58 +0200

    Add files WebSecurityConfig and UserRepositoryAuthProvider and update AppControllers to add user to the model ad manage the URI redirections.

---
+++ src/main/java/com/mumapp/mumapp/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.mumapp.mumapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public UserRepositoryAuthProvider userRepoAuthProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // Public pages
+        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers("/loginerror").permitAll();
+        http.authorizeRequests().antMatchers("/logout").permitAll();
+        //http.authorizeRequests().antMatchers("/dashboard").permitAll();
+
+
+        // Private pages (all other pages)
+        http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
+        http.authorizeRequests().antMatchers("/profile").hasAnyRole("ADMIN", "USER");
+        http.authorizeRequests().antMatchers("/dashboard").hasAnyRole("ADMIN", "USER");
+
+
+        // Login form
+        http.formLogin().loginPage("/login");
+        http.formLogin().usernameParameter("username");
+        http.formLogin().passwordParameter("password");
+        http.formLogin().defaultSuccessUrl("/dashboard");
+        http.formLogin().failureUrl("/loginerror");
+
+        // Logout
+        http.logout().logoutUrl("/logout");
+        http.logout().logoutSuccessUrl("/");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Database authentication provider
+        auth.authenticationProvider(userRepoAuthProvider);
+    }
+}
--- src/main/java/com/mumapp/mumapp/webapp/AppControllers.java
+++ src/main/java/com/mumapp/mumapp/webapp/AppControllers.java
@@ -1,11 +1,27 @@
 package com.mumapp.mumapp.webapp;
 
+import com.mumapp.mumapp.user.UserComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AppControllers {
+
+    @Autowired
+    private UserComponent userComponent;
+
+    @ModelAttribute
+    public void addUserToModel(Model model) {
+        boolean logged = userComponent.getLoggedUser() != null;
+        model.addAttribute("logged", logged);
+        if(logged) {
+            model.addAttribute("admin", userComponent.getLoggedUser().getRoles().contains("ROLE_ADMIN"));
+            model.addAttribute("userName",userComponent.getLoggedUser().getName());
+        }
+    }
 
     @GetMapping("/")
     public String index(Model model) {
@@ -14,7 +30,18 @@ public class AppControllers {
 
     @GetMapping("/login")
     public String login(Model model) {
+        model.addAttribute("hideLogin", true);
         return "login";
+    }
+
+    @GetMapping("/loginerror")
+    public String loginError() {
+        return "loginerror";
+    }
+
+    @GetMapping("/logout")
+    public String logout(Model model) {
+        return "index";
     }
 
     @GetMapping("/register")
